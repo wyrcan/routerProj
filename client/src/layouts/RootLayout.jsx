c8f4ab70d02d5c5e159d93ff2f5954005386e938
galic <galicia12mg@outlook.com>
2024-02-14 09:32:00 +0100

    rift1

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,5 +1,21 @@
 import { Outlet } from "react-router-dom";
 
 export function RootLayout() {
+  return (
+    <nav class="top-nav">
+      <div class="nav-text-large">My App</div>
+      <ul class="nav-list">
+        <li>
+          <a href="posts.html">Posts</a>
+        </li>
+        <li>
+          <a href="users.html">Users</a>
+        </li>
+        <li>
+          <a href="todos.html">Todos</a>
+        </li>
+      </ul>
+    </nav>
+  );
   return <Outlet />;
 }
